65110fff626452da5929c096bea106a45011a874
Jan Klopper <janklopper+underdark@gmail.com>
2025-08-20 13:30:43 +0200

    split nuclei raw files per normalizer
    Signed-off-by: Jan Klopper <janklopper+underdark@gmail.com>

--- boefjes/boefjes/plugins/kat_nuclei_cve/main.py
+++ boefjes/boefjes/plugins/kat_nuclei_cve/main.py
@@ -20,4 +20,4 @@ def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
     output = subprocess.run(cmd, capture_output=True)
     output.check_returncode()
 
-    return [({"openkat/nuclei-output"}, output.stdout.decode())]
+    return [({"openkat/nuclei-cve-output"}, output.stdout.decode())]
