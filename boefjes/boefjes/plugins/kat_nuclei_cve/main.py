06c098652479251bd31f7c4409b2bd66960b6797
Jan Klopper <janklopper+underdark@gmail.com>
2025-08-26 15:53:55 +0200

    Update main.py
    Signed-off-by: Jan Klopper <janklopper+underdark@gmail.com>

--- boefjes/boefjes/plugins/kat_nuclei_cve/main.py
+++ boefjes/boefjes/plugins/kat_nuclei_cve/main.py
@@ -1,20 +1,8 @@
 import subprocess
 
 
-def verify_hostname_meta(input_ooi):
-    # if the input object is HostnameHTTPURL then the hostname is located in netloc
-    if "netloc" in input_ooi and "name" in input_ooi["netloc"]:
-        netloc_name = input_ooi["netloc"]["name"]
-        port = input_ooi["port"]
-        return f"{netloc_name}:{port}"
-    else:
-        # otherwise the Hostname input object is used
-        return input_ooi["name"]
-
-
 def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
-    # Checks if the url is of object HostnameHTTPURL or Hostname
-    url = verify_hostname_meta(boefje_meta["arguments"]["input"])
+    url = boefje_meta["arguments"]["input"]["name"]
     cmd = ["/usr/local/bin/nuclei"] + boefje_meta["arguments"]["oci_arguments"] + ["-u", url]
 
     output = subprocess.run(cmd, capture_output=True)
