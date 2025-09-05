3b5959684b82d59ac0d02db9c26d0a34f7a508a8
ammar92 <ammar.abdulamir@gmail.com>
2025-09-05 15:57:57 +0200

    Remove url input for `kat_nuclei_cve` (#4761)

--- boefjes/boefjes/plugins/kat_nuclei_cve/main.py
+++ boefjes/boefjes/plugins/kat_nuclei_cve/main.py
@@ -1,6 +1,5 @@
 import subprocess
 
-
 def run(boefje_meta: dict) -> list[tuple[set, bytes | str]]:
     url = boefje_meta["arguments"]["input"]["name"]
     cmd = ["/usr/local/bin/nuclei"] + boefje_meta["arguments"]["oci_arguments"] + ["-u", url]
